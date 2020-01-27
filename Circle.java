74528db4d552ea5cbf9bd5585c5128822902bcd6
airi <59892547+airicognize@users.noreply.github.com>
2020-01-27 11:14:38 -0500

    Circles

---
+++ Circle.java
@@ -0,0 +1,17 @@
+public class Circle
+{
+	public static void main(String[] args)
+
+	{ 
+
+	  final double PI = 3.14159;
+	
+	 double radius = 3.5;
+	 double area;
+
+	 area = PI * radius * radius;
+
+	 System.out.println( "Calculating the radius of a 	circle which is 3.5.\n" + area);	
+   }
+
+}
