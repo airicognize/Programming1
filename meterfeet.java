4cfbd28dcd0d04ff0edd1685f765950af5c19ae4
airi <59892547+airicognize@users.noreply.github.com>
2020-01-31 00:24:01 -0500

    Update meterfeet.java

--- meterfeet.java
+++ meterfeet.java
@@ -2,11 +2,11 @@ public class meterfeet{
 
 	public static void main(String[] args){
 
-		double  converter, meter, feet;
+        double  converter, meter, feet;
 
-        feet = 3.281;
-		   meter = 5;
-	 converter = meter * feet;
+         feet = 3.281;
+        meter = 5;
+    converter = meter * feet;
 
 		System.out.println("\nThe distance of 5 units in meters is equal to " + converter + " feet.");
 
