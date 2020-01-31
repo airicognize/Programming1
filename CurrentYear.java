30e4d41fdd0a9480c17f0ec0154a162a1dbfaad0
Jeremie <trulyepicman2@gmail.com>
2020-01-31 00:03:33 -0500

    Hello

---
+++ CurrentYear.java
@@ -0,0 +1,19 @@
+public class CurrentYear{
+
+	public static void main(String[] args){
+
+		int yearOfBirth, MyAgeIs, currentYear;
+
+        MyAgeIs = 18;
+		
+		yearOfBirth = 2001;
+
+		currentYear = yearOfBirth + MyAgeIs + 1;
+
+		System.out.println("\nThe year is " + currentYear);
+		
+		System.out.println("\nI am " + MyAgeIs + " years old");
+
+		System.exit(0);
+	}
+}	
