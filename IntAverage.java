a6122acd01ea5e764fae2ad69732859de70023ef
airi <wow@gmail.com>
2020-02-03 10:27:55 -0500

    Integer Average

---
+++ IntAverage.java
@@ -0,0 +1,14 @@
+public class IntAverage{
+
+	public static void main(String[] args){
+
+	
+
+	 double averagenumber, divided;
+
+	 averagenumber = 1 + 7 + 9 + 34;
+	 divided = averagenumber/4;
+
+	 System.out.println("The average of 1, 7 ,9 and 34 is " + divided);
+    }
+}
